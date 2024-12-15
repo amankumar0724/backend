396cc87d8ed59c16087358a601aebeb777f86b86
amankumar0724 <ask5081811@gmail.com>
2024-12-15 06:41:30 +0530

    error fixed-utils/cloudinary.js

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 // WHAT IS THE USE OF THIS UTIL FILE?
 // initially files are uploaded on server, ab server se file ka jo path milega, uska use ham is "cloudinary.js" util me karenge taaki main us file ko apne server se cloudinary pe upload kar saku and remove that file from the server's local storage
-import {v2 as cloudinary} from 'cloudinary'
-import {fs} from 'fs'//file system = fs=>file handling
+import {v2 as cloudinary} from 'cloudinary';
+import {fs} from 'fs';//file system = fs=>file handling
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -9,7 +9,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadOnCloudinary = async (localFilePath) {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath)return null;
         // upload file on cloudinary
