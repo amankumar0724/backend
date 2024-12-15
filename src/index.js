2017931a00eae7ed7be45ae278cdd9b2f86c96fb
amankumar0724 <ask5081811@gmail.com>
2024-12-16 00:06:21 +0530

    INTRO: routes | usercontroller | postman

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import mongoose from 'mongoose'
 import { DB_NAME } from './constants.js'
 import connectDB from './db/index.js'
+import { app } from './app.js'
 import dotenv from 'dotenv'
 dotenv.config({
     path: './env'
